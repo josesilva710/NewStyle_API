fe1f62b3422aa100e90fb40e7f5f72fa980e7fb7
josesilva710 <silvajoseofc@gmail.com>
2026-05-21 23:04:07 -0300

    feat: adição de serializers para cadastramento e autenticação

--- Users/serializers.py
+++ Users/serializers.py
@@ -7,12 +7,41 @@ class AddressSerializer(serializers.ModelSerializer):
 
         model = Address
 
-        fields = ['id', 'user', 'rua', 'cidade', 'estado', 'cep']
+        fields = ['id', 'user', 'rua', 'cidade', 'estado', 'cep', 'numero']
+
+    def validate(self, data):
+        #Verifica se já existe um endereço igual para o mesmo usuário
+        if Address.objects.filter(
+            user=data['user'],
+            rua=data['rua'],
+            cidade=data['cidade'],
+            cep=data['cep']
+        ).exists():
+            raise serializers.ValidationError("Este endereço já está cadastrado para este usuário.")
+        return data
 
 class UsersSerializer(serializers.ModelSerializer):
+    
+    password = serializers.CharField(write_only=True)
+    
+    addresses = AddressSerializer(many=True, read_only=True)
+
     class Meta:
-
-        addresses = AddressSerializer(many = True, read_only = True)
-
         model = Users
-        fields = ['id', 'fullname', 'email', 'cliente_lojista', 'birthday', 'cpf', 'telephone', 'addresses']
+
+        fields = ['id', 'fullname', 'email', 'cliente_lojista', 'birthday', 'cpf', 'telephone', 'password', 'addresses']
+
+    #Reescrevendo o método que salva o objeto no banco de dados
+    def create(self, validated_data):
+        #metodo create_user para garantir a criptografia.
+        user = Users.objects.create_user(
+            email=validated_data['email'],
+            username=validated_data['email'],
+            fullname=validated_data['fullname'],
+            cliente_lojista=validated_data['cliente_lojista'],
+            birthday=validated_data.get('birthday'),
+            cpf=validated_data.get('cpf'),
+            telephone=validated_data.get('telephone'),
+            password=validated_data['password']
+        )
+        return user
