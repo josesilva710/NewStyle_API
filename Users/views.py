648932dc64ed96cb124d54a3ec8c76c3f0f1c094
josesilva710 <silvajoseofc@gmail.com>
2026-05-21 21:23:53 -0300

    feat: configura o viewsets do app Users

--- Users/views.py
+++ Users/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from Users.models import Users, Address
+from Users.serializers import UsersSerializer, AddressSerializer
+from rest_framework import viewsets
 
-# Create your views here.
+class UsersViewSet(viewsets.ModelViewSet):
+    queryset = Users.objects.all()
+    serializer_class = UsersSerializer
+
+class AddressViewSet(viewsets.ModelViewSet):
+    queryset = Address.objects.all()
+    serializer_class = AddressSerializer
+
