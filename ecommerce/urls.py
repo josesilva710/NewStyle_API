763f72e67374dd263bdc885cd4060f8e0067a3ad
josesilva710 <silvajoseofc@gmail.com>
2026-05-27 14:19:02 -0300

    feat: urls.py global reconfigurado

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,27 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import routers
-from Users.views import( 
-    UsersViewSet, 
-    AddressViewSet, 
-    LoginView, 
-    RegisterView,
-    ForgotPasswordView,
-    ResetPasswordView
-)
-
-router = routers.DefaultRouter()
-router.register(r'users', UsersViewSet)
-router.register(r'addresses', AddressViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    path('auth/register', RegisterView.as_view(), name='auth_register'),
-    path('auth/login', LoginView.as_view(), name='auth_login'),
-
-    path('auth/forgot-password', ForgotPasswordView.as_view(), name='auth_forgot_password'),
-    path('auth/reset-password', ResetPasswordView.as_view(), name='auth_reset_password'),
-
-    path('', include(router.urls)),
+    path('', include('Users.urls')),
+    path('', include('Shop.urls')),
 ]
