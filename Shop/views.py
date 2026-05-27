24b0c66bb130f7988067e0ebd681611c7a7e62b9
josesilva710 <silvajoseofc@gmail.com>
2026-05-26 22:51:20 -0300

    feat: iniciado views p/ a UC02

--- Shop/views.py
+++ Shop/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from Shop.models import produto, SKU
+from Shop.serializers import ProdutoSerializer, SKUSerializer
+from rest_framework import viewsets
 
-# Create your views here.
+class ProdutoViewSet(viewsets.ModelViewSet):
+
+    queryset = produto.objects.all()
+    serializer_class = ProdutoSerializer
+
+class SKUViewSet(viewsets.ModelViewSet):
+
+    queryset = SKU.objects.all()
+    serializer_class = SKUSerializer
