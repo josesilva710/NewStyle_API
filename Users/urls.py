d26082523fb18ad88379574354f3d0310ee7c4ad
josesilva710 <silvajoseofc@gmail.com>
2026-05-27 14:17:23 -0300

    feat: urls configurado para o app

---
+++ Users/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path, include
+from rest_framework import routers
+from Users.views import ( 
+    UsersViewSet, 
+    AddressViewSet, 
+    LoginView, 
+    RegisterView,
+    ForgotPasswordView,
+    ResetPasswordView
+)
+
+router = routers.DefaultRouter()
+router.register(r'users', UsersViewSet)
+router.register(r'addresses', AddressViewSet)
+
+urlpatterns = [
+    path('auth/register/', RegisterView.as_view(), name='auth_register'),
+    path('auth/login/', LoginView.as_view(), name='auth_login'),
+
+    path('auth/forgot-password/', ForgotPasswordView.as_view(), name='auth_forgot_password'),
+    path('auth/reset-password/', ResetPasswordView.as_view(), name='auth_reset_password'),
+
+    path('', include(router.urls)),
+]
