1745181f6c4f4cfc56db18cadd7c9c5dc47c8409
josesilva710 <silvajoseofc@gmail.com>
2026-05-29 18:43:49 -0300

    feat: URLs contato/atendimento/ e contato/suporte/

--- Users/urls.py
+++ Users/urls.py
@@ -6,12 +6,23 @@ from Users.views import (
     LoginView, 
     RegisterView,
     ForgotPasswordView,
-    ResetPasswordView
+    ResetPasswordView,
+    ContatoViewSet
 )
+from rest_framework_nested import routers
 
 router = routers.DefaultRouter()
 router.register(r'users', UsersViewSet)
 router.register(r'addresses', AddressViewSet)
+
+contact_router = routers.SimpleRouter()
+contact_router.register(r'contato', ContatoViewSet, basename='contato')
+
+suporte_router = routers.NestedSimpleRouter(contact_router, r'contato', lookup='contato')
+suporte_router.register(r'messages', ContatoViewSet, basename='contato-messages')
+
+atendimento_router = routers.NestedSimpleRouter(contact_router, r'contato', lookup='contato')
+atendimento_router.register(r'atendimento', ContatoViewSet, basename='contato-atendimento')
 
 urlpatterns = [
     path('auth/register/', RegisterView.as_view(), name='auth_register'),
@@ -19,6 +30,8 @@ urlpatterns = [
 
     path('auth/forgot-password/', ForgotPasswordView.as_view(), name='auth_forgot_password'),
     path('auth/reset-password/', ResetPasswordView.as_view(), name='auth_reset_password'),
+
+    path('', include(contact_router.urls)),
 
     path('', include(router.urls)),
 ]
