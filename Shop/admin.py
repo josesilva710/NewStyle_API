0691afa4c9950066e9f17d526465d81c3b77ae13
josesilva710 <silvajoseofc@gmail.com>
2026-06-05 14:40:20 -0300

    feat: registro do model carrinho e itemcarrinho

--- Shop/admin.py
+++ Shop/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import produto, SKU, Carrinho
+from .models import produto, SKU, Carrinho, ItemCarrinho
 
 admin.site.register(produto)
 admin.site.register(SKU)
 admin.site.register(Carrinho)
-
+admin.site.register(ItemCarrinho)
