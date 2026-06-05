dffbc8312abaea55be804c4f4a7273ef1ecdb7a9
josesilva710 <silvajoseofc@gmail.com>
2026-06-05 14:41:04 -0300

    feat: implementando o signals.py

--- Shop/apps.py
+++ Shop/apps.py
@@ -2,4 +2,8 @@ from django.apps import AppConfig
 
 
 class ShopConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'Shop'
+
+    def ready(self):
+        import Shop.signals 
