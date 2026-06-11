f8cb16e915a29ae87bbaa5edd32ceeed392e2d36
josesilva710 <newstyleapi@5e604d9b56f1>
2026-06-11 14:24:47 +0000

    feat: collectstatic do css do Django Rest

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 AUTH_USER_MODEL = 'Users.Users'
 
@@ -129,7 +130,7 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
 
-    'DEFAULT_PAGINATION_CLASS': 
+    'DEFAULT_PAGINATION_CLASS':
     'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
 
