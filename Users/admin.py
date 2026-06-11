4c4ea8f4c46b31486a5ca20193d7519d4ddfce33
josesilva710 <silvajoseofc@gmail.com>
2026-06-11 15:48:54 -0300

    refactor: atualizando os campos de endereços à otimização do model

--- Users/admin.py
+++ Users/admin.py
@@ -3,30 +3,22 @@ from django.contrib.auth.admin import UserAdmin
 from .models import Users, Address, Contact, PaymentMethodUser
 from django.db import models
 
-# Faz o endereço aparecer dentro da página do usuário
-class AddressInline(admin.StackedInline):
-    model = Address
-    extra = 1  # Quantidade de espaços em branco para novos endereços
-    classes = ['collapse'] # Deixa recolhido para não poluir visualmente
-
 @admin.register(Users)
 class MyUserAdmin(admin.ModelAdmin):
-    # Inlines: mostra os endereços do usuário na mesma tela
-    inlines = [AddressInline]
 
-    # O que aparece na LISTA principal (na tabela de usuários)
+    filter_horizontal = ['addresses']
+
     list_display = ('fullname', 'email', 'user_type', 'is_staff', 'is_active')
     
-    # Adiciona filtros na lateral direita (muito útil!)
     list_filter = ('user_type', 'is_staff', 'is_superuser', 'is_active')
 
-    # Define quais campos podem ser pesquisados na barra de busca
     search_fields = ('fullname', 'email', 'national_id')
 
     # Organiza os campos dentro da página de edição
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
         ('Informações Pessoais', {'fields': ('fullname', 'national_id', 'birthday', 'telephone')}),
+        ('Endereços', {'fields': ('addresses',)}),
         ('Tipo de Conta', {'fields': ('user_type',)}),
         ('Permissões', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
         ('Datas Importantes', {'fields': ('last_login',)}),
@@ -38,8 +30,14 @@ class MyUserAdmin(admin.ModelAdmin):
 # Registra o modelo Address para que ele apareça no admin
 @admin.register(Address)
 class AddressAdmin(admin.ModelAdmin):
-    list_display = ('id', 'street', 'city', 'state', 'cep', 'user')
-    search_fields = ('street', 'cep', 'user__email', 'user__fullname')
+    list_display = ('id', 'street', 'city', 'state', 'cep', 'get_users')
+    search_fields = ('street', 'cep', 'users__email', 'users__fullname')
+
+    def get_users(self, obj):
+
+        return ", ".join([user.fullname for user in obj.users.all()])
+    
+    get_users.short_description = 'Usuários Vinculados'
 
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
